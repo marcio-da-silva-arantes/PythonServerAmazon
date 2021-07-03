cf80d5f6b523c67dcfef8e0b2311448fe4034f14
Marcio da Silva Arantes <marcio.arantes@sc.senai.br>
2021-07-03 15:33:56 -0300

    update 03

--- application.py
+++ application.py
@@ -1,14 +1,13 @@
 from flask import Flask
 from waitress import serve
 
-app = Flask(__name__)
+application = Flask(__name__)
 
 
-@app.route('/')
+@application.route('/')
 def hello_world():
     return 'Maximiza Tecnologia: online Inteligence'
 
 if __name__ == '__main__':
-    serve(app, host='0.0.0.0', port=5000)  # WSGI serverpy
+    serve(application, host='0.0.0.0', port=5000)  # WSGI serverpy
 
-    

098fe6e9f77d989241c4ba617839c8b676d217e7
Marcio da Silva Arantes <marcio.arantes@sc.senai.br>
2021-07-03 15:08:55 -0300

    try - update to start server automaticaly on AWS

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from waitress import serve
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
     return 'Maximiza Tecnologia: online Inteligence'
 
+if __name__ == '__main__':
+    serve(app, host='0.0.0.0', port=5000)  # WSGI server
